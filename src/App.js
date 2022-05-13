65569a0ebf8c4f5655c7f53f6b6aeb89ac1d4cf7
CRSantiago <42612374+CRSantiago@users.noreply.github.com>
2022-05-12 21:23:10 -0400

    Design Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Info from './components/Info'
-import About from './components/About'
 import Links from './components/Links'
 
 function App() {
   return (
     <div className="App">
       <Info />
-      <About />
       <Links />
     </div>
   );
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import Image from '../images/IMG_2603.jpeg'
+import EmailIcon from '../images/email.png'
+import LinkedInIcon from '../images/linkedin.png'
 
 export default function Info(){
     return (
         <div>
-            <img src={Image} alt='luffy' className='info--logo' />
             <div className='info--headings'>
                 <h2>Christopher Santiago</h2>
                 <h3>Software Developer</h3>
             </div>
-            <div className='info--buttons'>
-                <button className='email--button'><a href='mailto:chrisantiago217@gmail.com'>Email</a></button>
-                <button className='linkedin--button'><a href='https://www.linkedin.com/in/christopher-a-santiago/'>LinkedIn</a></button>
+            <div className='info--icons'>
+            <a href='mailto:chrisantiago217@gmail.com'><img src={EmailIcon} alt="email icon"  className="contact--icon"/></a>
+            <a href='https://www.linkedin.com/in/christopher-a-santiago/'> <img src={LinkedInIcon} alt ="linkedin icon" className="contact--icon"/></a>
             </div>
         </div>
     )
